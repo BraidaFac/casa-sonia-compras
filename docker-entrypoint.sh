d2438575fb0e7862c3f7104a4c72bff8e9eb8210
BraidaFac <facundobraida98@gmail.com>
2026-07-26 13:50:17 +1200

    docker conifg

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,6 @@
 set -e
 
 echo "Running DB migrations..."
-node /app/prisma-cli/node_modules/.bin/prisma migrate deploy
+NODE_PATH=/app/prisma-cli/node_modules node /app/prisma-cli/node_modules/.bin/prisma migrate deploy
 
 exec node server.js
